1e7acf96342b87c2bd2852d26c6a399d5fee6d46
zyondias <zyon.dias@hotmail.com>
2013-08-12 11:57:01 -0300

    Confi. capistrano e outras coisas

---
+++ config/deploy.rb
@@ -0,0 +1,59 @@
+require 'bundler/capistrano'
+
+set :application, "192.168.0.60"
+
+set :scm, :git
+set :repository,  "https://github.com/zyondias/catarse.git"
+set :branch, 'master'
+set :deploy_via, :remote_cache
+
+set :keep_releases, 3
+
+set :user, 'webserver'
+set :runner, 'webserver'
+set :group, 'webserver'
+set :use_sudo, false
+
+set :deploy_to, '/var/www/catarse'
+set :current, "#{deploy_to}/current"
+
+default_run_options[:pty] = true
+
+ssh_options[:forward_agent] = true
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, application                          # Your HTTP server, Apache/etc
+role :app, application                          # This may be the same as your `Web` server
+role :db,  application, :primary => true # This is where Rails migrations will run
+#role :db,  application
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do
+    %w(config/database.yml).each do |path|
+      from  = "#{deploy_to}/#{path}"
+      to    = "#{current}/#{path}"
+
+      run "if [ -f '#{to}' ]; then rm '#{to}'; fi; ln -s #{from} #{to}"
+    end
+
+    run "cd #{current} && RAILS_ENV=production && GEM_HOME=/opt/local/ruby/gems && bundle exec unicorn_rails -c #{deploy_to}/config/unicorn.rb -D"
+  end
+
+  task :stop do
+    run "if [ -f #{deploy_to}/shared/pids/unicorn.pid ]; then kill `cat #{deploy_to}/shared/pids/unicorn.pid`; fi"
+  end
+
+  task :restart do
+    stop
+    start
+  end
+end
